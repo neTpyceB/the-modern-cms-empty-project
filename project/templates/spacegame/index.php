f43fa40ca87d9002213ce6653ab186793b7774c1
neTpyceB <pvman@inbox.ru>
2015-05-16 15:03:54 +0300

    Frontend templates folder

---
+++ project/templates/spacegame/index.php
@@ -0,0 +1,22 @@
+{%common%}
+<body>
+	<div id="wrapper">
+		<div id="content">
+			<div class="main">
+				COMMON HEADER {%common:common:header%}
+				<div class="content">
+                    PLUGIN TOP {%plugin:top%}
+                    <br>
+                    PLUGIN TEXT {%plugin:text%}
+                    <br>
+                    {%first%}
+                    <br>
+                    PLUGIN TEST {%plugin:test%}
+                    <br>
+                    PLUGIN BOTTOM IMAGE {%plugin:bottom_image%}
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">© <a href="http://devp.lv/">devp.lv</a></div>
+</body>
---
+++ project/templates/spacegame/registration.php
@@ -0,0 +1,14 @@
+{%common%}
+<body>
+	<div id="wrapper">
+		<div id="content">
+			<div class="main">
+				{%common:common:header%}
+				<div class="content">
+					{%registration:registration:index%}
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">© <a href="http://devp.lv/">devp.lv</a></div>
+</body>
