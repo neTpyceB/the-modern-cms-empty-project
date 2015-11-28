a5452be5845b58678de267fb0960d4b528d89218
neTpyceB <pvman@inbox.ru>
2015-11-28 09:38:37 +0200

    First template

--- project/migrations/2015_11_27_23_28_07.sql
+++ project/migrations/2015_11_27_23_28_07.sql
@@ -1,1 +1,3 @@
-TODO initial installation - create groups and default users, example pages and some entries in example module
+INSERT INTO `cms_languages` (`short`, `full`) VALUES ("en", "English");
+ALTER TABLE `cms_translations` ADD `en` TEXT;
+INSERT INTO `cms_pages` (`template_id`, `location`, `title`, `order`, `in_menu`, `active`) VALUES ("1", "en", "Welcome", "1", "1", "1");
