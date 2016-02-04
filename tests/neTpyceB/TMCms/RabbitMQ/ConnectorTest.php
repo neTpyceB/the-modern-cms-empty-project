cfe22e49cfdf48cbcdb2027f0dd3dcd0837c491d
neTpyceB <pvman@inbox.ru>
2016-02-04 16:42:37 +0200

    More tests, HTML, AgentDetector, PageHead, RabbitMQ

---
+++ tests/neTpyceB/TMCms/RabbitMQ/ConnectorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\TMCms\RabbitMQ;
+
+use TMCms\RabbitMQ\Connector;
+
+class ConnectorTest extends \PHPUnit_Framework_TestCase {
+    public function testGetConnection()
+    {
+//        $obj = Connector::getInstance();
+//        $connection = $obj->getConnection();
+
+//        $this->assertInstanceOf('AMQPConnection', $connection);
+    }
+}
