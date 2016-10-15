2dccb01bb5e9043f18e0469f86328a54a8f1e125
Vadims <pvman@inbox.ru>
2016-10-15 22:26:27 +0300

    Changed logo url to get more stats

--- configs/prod.php
+++ configs/prod.php
@@ -14,7 +14,7 @@ $config_prod = [
     'cms' => [
         'unique_key' => 'your_unique_key', // Required for updates
         'logo' => '/vendor/neTpyceB/TMCms/src/assets/images/logo.png', // Logo in admin panel
-        'logo_link' => 'http://devp.eu/', // Link on logo in admin panel
+        'logo_link' => 'http://devp.eu/?ref=devp_logo&project='. CFG_SITE_NAME, // Link on logo in admin panel
     ],
     'http_auth' => [
         'login' => '', // Set if required
