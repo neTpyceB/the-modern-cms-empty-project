6749646d83931ad502cfbdc7e56eeeefb8b21cbd
neTpyceB <pvman@inbox.ru>
2017-06-28 23:34:25 +0300

    boot file refactored to be more php7 friendly

--- boot.php
+++ boot.php
@@ -1,23 +1,26 @@
 <?php
+declare(strict_types=1);
 
 use TMCms\Config\Configuration;
 use TMCms\Config\Settings;
+use TMCms\Log\Errors;
 
 defined('INC') or exit;
 
 // Path of root folder
-define('DIR_BASE', str_replace('\\', '/', dirname(__FILE__)) . '/');
+define('DIR_BASE', str_replace('\\', '/', __DIR__) . '/');
 
 // Using Composer
-$autoloader_file = DIR_BASE . 'vendor/autoload.php';
-if (file_exists($autoloader_file)) {
-    require_once $autoloader_file;
+$autoload_file = DIR_BASE . 'vendor/autoload.php';
+if (file_exists($autoload_file)) {
+    require_once $autoload_file;
 } else {
-    die('Vendor libraries not found.<br>Run composer -v update in console.<br>Autoloader must be in "' . $autoloader_file . '"');
+    die('Vendor libraries not found.<br>Run composer -v update in console.<br>Autoload must be in "' . $autoload_file . '"');
 }
 
-// Error handler
-set_error_handler(array('\TMCms\Log\Errors', 'Handler'));
+// Error and exception handler
+set_error_handler([Errors::class, 'Handler']);
+set_exception_handler([Errors::class, 'Handler']);
 
 $config = Configuration::getInstance();
 
@@ -25,22 +28,20 @@ $config->addConfigurationEnv(); // Set default
 
 // Check auth
 $auth = Configuration::getInstance()->get('http_auth');
-$login = isset($auth['login']) ? $auth['login'] : '';
-$password = isset($auth['password']) ? $auth['password'] : '';
-if ($login && $password) {
-    if (!isset($_SERVER['PHP_AUTH_USER']) || (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) || ($_SERVER['PHP_AUTH_USER'] != $login || $_SERVER['PHP_AUTH_PW'] != $password)) {
-        header('WWW-Authenticate: Basic realm="Authentication System"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo "No access";
-        exit;
-    }
+$login = $auth['login'] ?? '';
+$password = $auth['password'] ?? '';
+if (($login && $password) && (!isset($_SERVER['PHP_AUTH_USER']) || (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) || ($_SERVER['PHP_AUTH_USER'] !== $login || $_SERVER['PHP_AUTH_PW'] !== $password))) {
+    header('WWW-Authenticate: Basic realm="Authentication System"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'No access';
+    exit;
 }
 
+// Init Settings
 Settings::getInstance()->init();
 
 // Hide all errors in prod env
 if (Settings::isProductionState()) {
-    error_reporting(0);
     ini_set('display_errors', false);
     ini_set('display_startup_errors', false);
 }
