92a9cac8d7727457252c303efbb35b33deab2080
neTpyceB <pvman@inbox.ru>
2017-07-28 19:23:56 +0300

    Config merge with return result

--- configs/prod.php
+++ configs/prod.php
@@ -26,7 +26,7 @@ $config_prod = [
 // Change all your local settings in local.php - file should return array with fields to be overwritten
 $config_local = [];
 if (file_exists(__DIR__ . '/local.php')) {
-    $config_local = include_once __DIR__ . '/local.php';
+    $config_local = include __DIR__ . '/local.php';
 }
 
 return array_replace_recursive($config_prod, $config_local);
