cd008647d7dd5a4a601bed3a953bcb57159367c5
DEVP Web Development <info@devp.eu>
2018-08-25 09:48:18 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -12,31 +12,36 @@ define('DIR_BASE', str_replace('\\', '/', __DIR__) . '/');
 
 // Using Composer
 $autoload_file = DIR_BASE . 'vendor/autoload.php';
-if (file_exists($autoload_file)) {
-    require_once $autoload_file;
-} else {
-    die('Vendor libraries not found.<br>Run composer -v update in console.<br>Autoload must be in "' . $autoload_file . '"');
+if (!file_exists($autoload_file)) {
+    die('Vendor libraries not found. Run command <code style="background-color: #000; color: #fff">composer -v update</code> in console.');
 }
+require_once $autoload_file;
 
 // Error and exception handler
 set_error_handler([Errors::class, 'Handler']);
 
+// Load default configuration
 $config = Configuration::getInstance();
-
-$config->addConfigurationEnv(); // Set default
+$config->addConfigurationEnv();
 
 // Check auth
 $auth = Configuration::getInstance()->get('http_auth');
 $login = $auth['login'] ?? '';
 $password = $auth['password'] ?? '';
-if (($login && $password) && (!isset($_SERVER['PHP_AUTH_USER']) || (!isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) || ($_SERVER['PHP_AUTH_USER'] !== $login || $_SERVER['PHP_AUTH_PW'] !== $password))) {
+if (
+    ($login && $password) // If login and password are set in config...
+    && ( // ... we check that same are supplied by user
+        !isset($_SERVER['PHP_AUTH_USER'])
+        || !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])
+        || ($_SERVER['PHP_AUTH_USER'] !== $login || $_SERVER['PHP_AUTH_PW'] !== $password))
+    ) {
     header('WWW-Authenticate: Basic realm="Authentication System"');
     header('HTTP/1.0 401 Unauthorized');
-    echo 'No access';
-    exit;
+
+    die('No access');
 }
 
-// Init Settings
+// Init settings from admin panel
 Settings::getInstance()->init();
 
 // Hide all errors in prod env
